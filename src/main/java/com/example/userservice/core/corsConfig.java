0c6dacecd077a609b498138a00b7dfc78dc84bf4
PR800 <64070025@it.kmitl.ac.th>
2023-11-12 02:10:47 +0700

    corsConfig

--- src/main/java/com/example/userservice/core/corsConfig.java
+++ src/main/java/com/example/userservice/core/corsConfig.java
@@ -1,4 +1,4 @@
-package com.example.rankingbook.command;
+package com.example.userservice.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
